139110e345db0119c5e1fa3e96b6dcef307acca8
acaciam <89486490+acaciam@users.noreply.github.com>
2022-12-08 22:42:13 -0500

    changed decOutput file and updated the .bdf file
    The clr functionality is still not working

--- DDS_Final/decOutput.v
+++ DDS_Final/decOutput.v
@@ -1,22 +1,72 @@
-module decOutput(dec, clk, lights);
-	input [3:0]dec;
+module decOutput(dec, clr, clk, lights);
+	input [15:0]dec;
+	input clr;
 	input clk;
-	output reg [6:0] lights;
-
-	always@(posedge clk)
-
-	//convert to output dec to 7-seg display
-		case(dec)
-				4'h0 : lights = 7'b1000000;
-				4'h1 : lights = 7'b1111001;
-				4'h2 : lights = 7'b0100100;
-				4'h3 : lights = 7'b0110000;		
-				4'h4 : lights = 7'b0011001;
-				4'h5 : lights = 7'b0010010;			
-				4'h6 : lights = 7'b0000010;
-				4'h7 : lights = 7'b1111000;			
-				4'h8 : lights = 7'b0000000;
-				4'h9 : lights = 7'b0011000;	
-		endcase
+	output reg [47:0] lights;
+	
+	
+	always@(posedge clk) begin
+		if(clr) begin
+			lights[47:0] <= 48'hffffffffffff;
+		end
+		else if(!clr) begin
+			//output 100th's place
+			lights[7:0] = 8'b11000000;
+			//output 10th's place
+			lights[15:8] = 8'b11000000;
+			//output 1's place
+			case(dec % 10)
+					4'h0 : lights[23:16] = 8'b01000000;
+					4'h1 : lights[23:16] = 8'b01111001;
+					4'h2 : lights[23:16] = 8'b00100100;
+					4'h3 : lights[23:16] = 8'b00110000;		
+					4'h4 : lights[23:16] = 8'b00011001;
+					4'h5 : lights[23:16] = 8'b00010010;			
+					4'h6 : lights[23:16] = 8'b00000010;
+					4'h7 : lights[23:16] = 8'b01111000;			
+					4'h8 : lights[23:16] = 8'b00000000;
+					4'h9 : lights[23:16] = 8'b00011000;	
+			endcase
+			//output 10's place
+			case((dec/10) % 10)
+					4'h0 : lights[31:24] = 8'b11000000;
+					4'h1 : lights[31:24] = 8'b11111001;
+					4'h2 : lights[31:24] = 8'b10100100;
+					4'h3 : lights[31:24] = 8'b10110000;		
+					4'h4 : lights[31:24] = 8'b10011001;
+					4'h5 : lights[31:24] = 8'b10010010;			
+					4'h6 : lights[31:24] = 8'b10000010;
+					4'h7 : lights[31:24] = 8'b11111000;			
+					4'h8 : lights[31:24] = 8'b10000000;
+					4'h9 : lights[31:24] = 8'b10011000;	
+			endcase
+			//output 100's place
+			case((dec/100) % 10)
+					4'h0 : lights[39:32] = 8'b11000000;
+					4'h1 : lights[39:32] = 8'b11111001;
+					4'h2 : lights[39:32] = 8'b10100100;
+					4'h3 : lights[39:32] = 8'b10110000;		
+					4'h4 : lights[39:32] = 8'b10011001;
+					4'h5 : lights[39:32] = 8'b10010010;			
+					4'h6 : lights[39:32] = 8'b10000010;
+					4'h7 : lights[39:32] = 8'b11111000;			
+					4'h8 : lights[39:32] = 8'b10000000;
+					4'h9 : lights[39:32] = 8'b10011000;	
+			endcase
+			//output 1000's place
+			case((dec/1000) % 10)
+					4'h0 : lights[39:32] = 8'b11000000;
+					4'h1 : lights[39:32] = 8'b11111001;
+					4'h2 : lights[39:32] = 8'b10100100;
+					4'h3 : lights[39:32] = 8'b10110000;		
+					4'h4 : lights[39:32] = 8'b10011001;
+					4'h5 : lights[39:32] = 8'b10010010;			
+					4'h6 : lights[39:32] = 8'b10000010;
+					4'h7 : lights[39:32] = 8'b11111000;			
+					4'h8 : lights[39:32] = 8'b10000000;
+					4'h9 : lights[39:32] = 8'b10011000;	
+			endcase
+		end
+		end
 	
 endmodule
--- DDS_Final/phase_accum.v
+++ DDS_Final/phase_accum.v
@@ -10,12 +10,14 @@ module phase_accum(clk, clr, sw, set, fout, decVal);
 	always
 	begin
 		if(clr) begin
-			decVal <= 0;
-			tuner <= 0;
+			decVal = 0;
+			tuner = 0;
 		end
-		else if(!set) begin
-			decVal <= sw[0]*1 + sw[1]*2 + sw[2]*4 + sw[3]*8 + sw[4]*16 + sw[5]*32 + sw[6]*64 + sw[7]*128 + sw[8]*256 + sw[9]*512;
-			tuner <= baseTune * decVal;
+		else begin
+			if(!set) begin
+				decVal <= sw[0]*1 + sw[1]*2 + sw[2]*4 + sw[3]*8 + sw[4]*16 + sw[5]*32 + sw[6]*64 + sw[7]*128 + sw[8]*256 + sw[9]*512;
+				tuner <= baseTune * decVal;
+			end
 		end
 	end	
 	always@(posedge clk) 
