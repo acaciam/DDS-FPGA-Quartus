33b6cfc3be8d758b4d63b7f66072bbae0858bcd0
acaciam <89486490+acaciam@users.noreply.github.com>
2022-12-14 22:09:38 -0500

    minimized LUT to 1/4 size
    Implemented minimization of LUTs with two separate verilog functions. One to update the phase_accumulator input to the LUT (keep it between 0 and 256). The other change the output from the LUT by mirroring when necessary.
    I tried testing to confirm my work but kept getting the same output on the Signal Tap Analyzer. I tried debugging by using constant values as inputs to the MUX instead of the SIN and TRI LUT outputs. The error seems to be in switching between outputs with the MUX. Maybe something is wrong with my GPIO connectors?

---
+++ DDS_Final/changeFout.v
@@ -0,0 +1,14 @@
+module changeFout(fin, clk, fout);
+	input [9:0] fin;
+	input clk;
+	output reg [7:0] fout;
+	
+	always @(posedge clk)
+	begin
+		if(fin >= 0 && fin < 256) fout =  fin;
+		else if(fin >= 256 && fin < 512) fout =  511 - fin;
+		else if(fin >= 512 && fin < 768) fout = fin - 512;
+		else if(fin >= 768 && fin < 1024) fout =  1023 - fin;
+	end
+	
+endmodule
--- DDS_Final/sinLUT_bb.v
+++ DDS_Final/sinLUT_bb.v
@@ -37,7 +37,7 @@ module sinLUT (
 	clock,
 	q);
 
-	input	[9:0]  address;
+	input	[7:0]  address;
 	input	  clock;
 	output	[9:0]  q;
 `ifndef ALTERA_RESERVED_QIS
@@ -71,14 +71,14 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
 // Retrieval info: PRIVATE: MIFfilename STRING "sinLUT.mif"
-// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "1024"
+// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "256"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
 // Retrieval info: PRIVATE: RegOutput NUMERIC "1"
 // Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 // Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 // Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
-// Retrieval info: PRIVATE: WidthAddr NUMERIC "10"
+// Retrieval info: PRIVATE: WidthAddr NUMERIC "8"
 // Retrieval info: PRIVATE: WidthData NUMERIC "10"
 // Retrieval info: PRIVATE: rden NUMERIC "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
@@ -89,17 +89,17 @@ endmodule
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "MAX 10"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
-// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "1024"
+// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "256"
 // Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
 // Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: OUTDATA_REG_A STRING "CLOCK0"
-// Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "10"
+// Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "8"
 // Retrieval info: CONSTANT: WIDTH_A NUMERIC "10"
 // Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
-// Retrieval info: USED_PORT: address 0 0 10 0 INPUT NODEFVAL "address[9..0]"
+// Retrieval info: USED_PORT: address 0 0 8 0 INPUT NODEFVAL "address[7..0]"
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
 // Retrieval info: USED_PORT: q 0 0 10 0 OUTPUT NODEFVAL "q[9..0]"
-// Retrieval info: CONNECT: @address_a 0 0 10 0 address 0 0 10 0
+// Retrieval info: CONNECT: @address_a 0 0 8 0 address 0 0 8 0
 // Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
 // Retrieval info: CONNECT: q 0 0 10 0 @q_a 0 0 10 0
 // Retrieval info: GEN_FILE: TYPE_NORMAL sinLUT.v TRUE
